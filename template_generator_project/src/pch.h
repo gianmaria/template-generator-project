5437a3415eab54ede2d652b1863a9bcc64bb6d04
Gianmaria <gianmaria.bonici@gmail.com>
2022-12-09 10:13:10 +0100

    add more include

--- template_generator_project/src/pch.h
+++ template_generator_project/src/pch.h
@@ -1,12 +1,38 @@
 #pragma once
 
+#include <algorithm>
+#include <chrono>
+#include <exception>
 #include <filesystem>
+#include <format>
 #include <fstream>
+#include <future>
 #include <iostream>
+#include <list>
+#include <mutex>
+#include <optional>
+#include <random>
+#include <sstream>
+#include <stdexcept>
+#include <string_view>
 #include <string>
+#include <system_error>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <variant>
 #include <vector>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::string_view;
+using std::optional;
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
 namespace fs = std::filesystem;
+
+using str = std::string;
+using str_cref = std::string const &;
