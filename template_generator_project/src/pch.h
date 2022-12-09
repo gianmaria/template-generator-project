b68a14e646d93f415715b75b09abacbb6c12e362
Gianmaria <gianmaria.bonici@gmail.com>
2022-12-09 10:53:47 +0100

    more header i guess

--- template_generator_project/src/pch.h
+++ template_generator_project/src/pch.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
 #include <chrono>
 #include <exception>
 #include <filesystem>
@@ -9,9 +10,12 @@
 #include <future>
 #include <iostream>
 #include <list>
+#include <map>
 #include <mutex>
+#include <numeric>
 #include <optional>
 #include <random>
+#include <set>
 #include <sstream>
 #include <stdexcept>
 #include <string_view>
@@ -27,7 +31,7 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::string_view;
-using std::optional;
+using std::vector;
 
 using namespace std::string_literals;
 using namespace std::string_view_literals;
