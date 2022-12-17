77958a37511a537857c3ce96322a06adeae1154b
Gianmaria <gianmaria.bonici@gmail.com>
2022-12-17 12:41:20 +0100

    even more header

--- template_generator_project/src/pch.h
+++ template_generator_project/src/pch.h
@@ -17,6 +17,7 @@
 #include <random>
 #include <set>
 #include <sstream>
+#include <stack>
 #include <stdexcept>
 #include <string_view>
 #include <string>
@@ -26,6 +27,17 @@
 #include <utility>
 #include <variant>
 #include <vector>
+#include <queue>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 
 using std::cout;
 using std::endl;
