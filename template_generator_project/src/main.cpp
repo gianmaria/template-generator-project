a61d0c83d9fd3edfd549a99a6a655c06e348d9a7
Gianmaria <gianmaria.bonici@gmail.com>
2023-08-11 15:59:29 +0200

    removed unused includes

--- template_generator_project/src/main.cpp
+++ template_generator_project/src/main.cpp
@@ -1,31 +1,5 @@
-#include <algorithm>
-#include <array>
-#include <chrono>
-#include <exception>
-#include <filesystem>
-#include <format>
-#include <fstream>
-#include <future>
 #include <iostream>
-#include <list>
-#include <map>
-#include <mutex>
-#include <numeric>
-#include <optional>
-#include <queue>
-#include <random>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <string_view>
-#include <string>
-#include <system_error>
-#include <thread>
-#include <unordered_map>
-#include <utility>
-#include <variant>
-#include <vector>
+#include <cstdint>
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -39,17 +13,6 @@ using i64 = int64_t;
 
 using std::cout;
 using std::endl;
-using std::string;
-using std::string_view;
-using std::vector;
-
-using namespace std::string_literals;
-using namespace std::string_view_literals;
-
-namespace fs = std::filesystem;
-
-using str = std::string;
-using str_cref = std::string const &;
 
 int main()
 {
