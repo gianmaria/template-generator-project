0d9aa63e192273d41c14b4b733c9a6a365a4f4ac
Gianmaria <gianmaria.bonici@gmail.com>
2023-12-08 19:34:58 +0100

    spiciest version, inspired by the work on AOC

--- template_generator_project/src/main.cpp
+++ template_generator_project/src/main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+#include <array>
+#include <assert.h>
 #include <cstdint>
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <regex>
+#include <set>
+#include <string_view>
+#include <string>
+#include <vector>
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -13,10 +34,26 @@ using i64 = int64_t;
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::string_view;
+using std::vector;
+using std::set;
+using std::map;
+
+using str = std::string;
+using str_cref = std::string const&;
+
+template<typename T>
+using vec = vector<T>;
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+namespace fs = std::filesystem;
+
 
 int main()
 {
-    cout << "hello world" << endl;
-
+    cout << "hello" << endl;
     return 0;
 }
